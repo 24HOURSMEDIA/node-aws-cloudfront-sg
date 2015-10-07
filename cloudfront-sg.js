b41edbff8030a7a85d28ef551a5eb20712f49204
24hoursmedia <info@24hoursmedia.com>
2015-10-07 18:17:47 +0200

    arguments security group ids

--- cloudfront-sg.js
+++ cloudfront-sg.js
@@ -1,3 +1,16 @@
 #!/usr/bin/env node
 
-console.log('hello world');
+var argv = require('optimist')
+    .usage('Update AWS security groups with cloudfront IP\'s.\nUsage: $0 arg1..argn')
+    .demand(1)
+
+    .describe('arg1..argn', 'One or more security group ID\'s')
+    .argv;
+
+argv.securityGroups = argv._;
+
+console.log('Update AWS security groups %s with cloudfront IP\'s', argv.securityGroups.join(',', argv.securityGroups));
+
+
+
+
