3f33a757d1b749e6f922f82d099bacd04dba5d94
24hoursmedia <info@24hoursmedia.com>
2015-10-07 18:45:58 +0200

    retrieve cloudfront ip list

--- cloudfront-sg.js
+++ cloudfront-sg.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 
+var Seq = require('seq');
+
 var argv = require('optimist')
     .usage('Update AWS security groups with cloudfront IP\'s.\nUsage: $0 arg1..argn')
     .demand(1)
@@ -8,6 +10,34 @@ var argv = require('optimist')
     .argv;
 
 argv.securityGroups = argv._;
+
+var cloudfrontIps = {};
+
+Seq()
+    .par(function () {
+        var _self = this;
+        var request = require("request");
+        request('https://ip-ranges.amazonaws.com/ip-ranges.json', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                //   console.log(body) // Show the HTML for the Google homepage.
+                 cloudfrontIps = JSON.parse(response.body);
+                _self();
+            } else {
+                _self("Can't divide by zero");
+            }
+        })
+    })
+    .par(function () {
+        var _self = this;
+        console.log('par');
+        _self();
+    })
+    .seq(function () {
+        console.log('Loaded');
+        console.log(cloudfrontIps.prefixes);
+    }).catch(function (err) {
+        console.error(err.stack ? err.stack : err)
+    });
 
 console.log('Update AWS security groups %s with cloudfront IP\'s', argv.securityGroups.join(',', argv.securityGroups));
 
